d8efcc3bb90510179ff7b3669ce9f1e8f556379c
Gad <gad.programming@gmail.com>
2026-07-30 00:33:34 +0200

    game class

---
+++ game.py
@@ -0,0 +1,22 @@
+import pygame,sys
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("ninja game")#change name of the window
+        self.screen = pygame.display.set_mode((640,480)) #function that creates the screen
+        self.clock = pygame.Clock()#Clock object
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+
+Game().run()
